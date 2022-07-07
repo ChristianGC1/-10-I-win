e909e3f0b0c95b5c7d412a0fa94b565b7ce3d961
ChristianGC1 <1018833@student.sae.edu.au>
2022-07-07 20:51:19 +1000

    Initial commit

---
+++ Assets/2D PIxel/2D SideScroller Pixel Art Kit/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //public GameObject gameOverCanvas;
+    public GameObject youWinCanvas;
+
+    public void Start()
+    {
+        Time.timeScale = 1;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            doExitGame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Replay();
+        }
+    }
+
+    public void doExitGame()
+    {
+        Debug.Log("Quitting Game!");
+        Application.Quit();
+    }
+
+    public void GameOver()
+    {
+        Invoke("End", 0.25f);
+    }
+
+    public void YouWin()
+    {
+        Invoke("Win", 0.25f);
+    }
+
+    //private void End()
+    //{
+    //    gameOverCanvas.SetActive(true);
+    //    Time.timeScale = 0;
+    //}
+
+    private void Win()
+    {
+        youWinCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
